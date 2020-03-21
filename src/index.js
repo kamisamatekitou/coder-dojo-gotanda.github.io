1f903fc12e45a71b94ca7aa4fb114ac2f4905540
Takeru Ichii <t.ichii@c-fo.com>
2020-03-21 16:01:19 +0900

    mount react app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const dom = document.getElementById('app');
+
+const Main = (_props) => {
+  return <div>codadojo gotanda</div>
+};
+
+ReactDOM.render(<Main/>, dom);
